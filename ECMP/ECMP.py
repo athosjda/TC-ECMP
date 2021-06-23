50a310c2a3fc5fb3313dd4a7ab9c779f136f037a
athosjda <athosjda@gmail.com>
2021-06-23 12:20:24 -0300

    Add array de probabilidade.

--- ECMP/ECMP.py
+++ ECMP/ECMP.py
@@ -8,6 +8,10 @@ class ECMP:
         self.G = G
         self.a = np.random.randint(2, size=G.m)
         self.d = np.zeros(G.n)
+        self.b = [(1/G.m)*i for i in range(G.m + 1)]
+        self.s = sum(self.a)
+        
+        self._ultimo_bit = -1
 
     def ea_1p1(self):
         s = 0
