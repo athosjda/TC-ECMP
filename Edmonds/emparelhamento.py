f1b9305a73fbf06c374d7bdb2868c76a4ce996ff
athosjda <athosjda@gmail.com>
2022-08-27 16:06:38 -0300

    Adição do método "caminho_aumentante".

--- Edmonds/emparelhamento.py
+++ Edmonds/emparelhamento.py
@@ -1,23 +1,49 @@
+from operator import index
 from Grafo import Grafo
 
 class Emparelhamento:
     def __init__(self, G:Grafo):
         self._G = G
         self._M = []
-        self._P = []
 
     def emperelhamento(self):
-        self.M = [None]*(G.n+1)
+        self._M = [None]*(G.n+1)
         
         while True:
-            self._P = reducao_flor()
-            self._M = diferenca_simentrica()
-            if len(self._P) == 0:
-                break
-            return self._M
+            p = self.caminho_aumentante()
+            if len(p) == 0:
+                return self._M    
+            
+    def caminho_aumentante(self):
+        F = []
+        E = []
+        P = []
+        raiz = [-1]*self._G.n
+        distancia = [-1]*self._G.n
+        for v in self._G.vertex:
+            if v not in self._M:
+                F.append(v)
+                raiz[v] = v
+                distancia[v] = 0
+        for e in self._G.edges:
+            E.append(e)
+        for e in self._M:
+            if e in self._M:
+                E.remove(e)
+        for v in F:
+            for e in E:
+                if e['w'] not in F:
+                    # adicionar na floresta
+                    pass
+                else:
+                    if distancia[e['w']]%2 == 0:
+                        if raiz[e['v']] != raiz[e['w']]:
+                            # retorna o caminho aumentante
+                            pass
+                        else:
+                            # resolve o "broto" haha
+                            pass
+                        return P
+                E.remove(e)
+        return P
 
-    def reducao_flor():
-        return []
-
-    def diferenca_simetrica():
-        return []
--- Grafo.py
+++ Grafo.py
@@ -1,6 +1,7 @@
 class Grafo:
     def __init__(self, n, direcionado=False):
         self.n = n
+        self.vertex = [v for v in range(0, n)]
         self.m = 0
         self.edges = []
         self.adj = [[] for _ in range(n)]
@@ -9,7 +10,7 @@ class Grafo:
 
     def inserir_arestas(self, u, v):
         self.adj[u].append(v)
-        self.edges.append((u, v))
+        self.edges.append({'v':u, 'w':v})
         if not self._direcionado:
             self.adj[v].append(u)
         self.m += 1
