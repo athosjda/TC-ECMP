d37fac1a9c47a0a1e2cbec8a4d214dd7970d7d48
athosjda <athosjda@gmail.com>
2023-02-11 17:25:11 -0300

    classe de grafos conforme a ref

--- Grafo.py
+++ Grafo.py
@@ -1,16 +1,22 @@
-class Grafo:
-    def __init__(self, n, direcionado=False):
+class Grafo(object):
+    def __init__(self, n, orientado=False):
+        self.n = None
+        self.m = None
+        self.__orientado = orientado
+
+    def set_n(self, n):
         self.n = n
-        self.vertex = [v for v in range(0, n)]
         self.m = 0
-        self.edges = []
-        self.adj = [[] for _ in range(n)]
+    
+    def V(self):
+        return [i for i in range(1, self.n+1)]
 
-        self._direcionado = direcionado
-
-    def inserir_arestas(self, u, v):
-        self.adj[u].append(v)
-        self.edges.append({'v':u, 'w':v})
-        if not self._direcionado:
-            self.adj[v].append(u)
-        self.m += 1
+    def E(self, iterar_sobre_no=False):
+        for v in self.V():
+            for w in self.N(v, tipo = '+' if self.__orientado else '*', iterar_sobre_no = iterar_sobre_no):
+                enumerar = True
+                if not self.__orientado:
+                    wint = w if isinstance(w, int) else w.viz
+                enumerar = v < wint
+        if enumerar:
+            yield (v, w)
