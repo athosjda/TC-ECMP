d4bedd6659dcbe376f98605018202e52cb0ba787
athosjda <athosjda@gmail.com>
2023-02-12 14:30:40 -0300

    classes de nó e aresta

---
+++ aresta.py
@@ -0,0 +1,4 @@
+class aresta(object):
+    def __init__(self):
+        self.v1, self.No1 = None, None
+        self.v2, self.No2 = None, None
---
+++ no.py
@@ -0,0 +1,5 @@
+class no(object):
+    def __inti__(self):
+        self.vizinho = None
+        self.e = None
+        self.proximo = None
