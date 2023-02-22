ec8ebd55484f87237cdee1305efcac52912d2905
athosjda <athosjda@gmail.com>
2023-02-22 19:04:09 -0300

    função de inicialização e avaliação

--- ECMP/ECMP.py
+++ ECMP/ECMP.py
@@ -1,36 +1,31 @@
-from Grafo import Grafo
-import scipy.special as spy
+import random as rnd
 
-import numpy as np
+from GrafoListaAdjacencia import GrafoListaAdjacencia as Grafo
 
-class ECMP:
-    def __init__(self, G:Grafo):
-        self.G = G
-        self.a = np.random.randint(2, size=G.m)
-        self.d = np.zeros(G.n)
-        self.b = [(1/G.m)*i for i in range(G.m + 1)]
-        self.s = sum(self.a)
-        
-        self._ultimo_bit = -1
+def ECMP(G:Grafo):
+    #rnd.seed(1)
+    M = inicializacao(G)
+    fit = avaliacao(M, G)
+    return (M, fit)
 
-    def ea_1p1(self):
-        s = 0
-        c = 0
-        
-        for idx, edge in enumerate(self.G.edges):
-            s += self.a[idx]
-            self.d[edge[0]] += 1
-            self.d[edge[1]] += 1
-        
-        for ele in self.d:
-            c += spy(ele, 2)
+def inicializacao(G:Grafo):
+    keys = [k for k in G.E()]
+    values = [rnd.randint(0, 1) for _ in range(len(keys))]
+    return dict(zip(keys, values))
 
-        return c > 0 and -c or s
+def avaliacao(M, G:Grafo):
+    saturado = [0]*(G.n+1)
+    fit = 0
+    for bit in M:
+        if (M[bit] == 1):
+            fit = fit+1
+            saturado[bit[0]] = saturado[bit[0]]+1
+            saturado[bit[1]] = saturado[bit[1]]+1
+    
+    for idx in saturado:
+        print(idx)
+        if (idx >= 2):
+            fit = fit*(-1)
+            break
 
-    def _esta_entre(x, l, r):
-        return l <= x and x < r
-
-    def linear_search(self, x):
-        for i in range(1, len(self.b)):
-            if self._esta_entre(x, self.b[i-1], self.b[i]):
-                return i-1
+    return fit
